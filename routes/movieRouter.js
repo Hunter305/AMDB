a85c7e4d66098997079b586d1fe7069f6455406d
Hunter305 <avinash8169@gmail.com>
2024-02-21 15:08:09 +0530

    added movies controller

--- routes/movieRouter.js
+++ routes/movieRouter.js
@@ -1,5 +1,14 @@
 import express from "express";
+import { createMovie, deleteMovie, findMovies, updateMovie } from "../controllers/movieController";
 
 const router = express.Router();
+
+router.post("/movies", createMovie);
+
+router.get("/movies", findMovies);
+
+router.patch("/movies/:id", updateMovie);
+
+router.delete("/movies/:id", deleteMovie);
 
 export default router;
