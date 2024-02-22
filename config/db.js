61c8321b170d9e7c6c83a0b771eb3d82fdbe1336
Hunter305 <avinash8169@gmail.com>
2024-02-22 20:15:27 +0530

    filtered only the frontend

--- config/db.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const db = async () => {
-  const conn = await mongoose.connect(process.env.MONGO);
-  console.log(`database connected at ${conn.connection.host}`);
-};
-
-export default db;
